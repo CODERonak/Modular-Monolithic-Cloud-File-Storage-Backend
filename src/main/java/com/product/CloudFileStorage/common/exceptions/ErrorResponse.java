bc8c021f49a3425814fc665cbe4b35876c106a2d
r9988.2588 <r9988.2588@gmail.com>
2026-05-11 05:05:19 +0000

    feat(exception): add GlobalExceptionHandler

---
+++ src/main/java/com/product/CloudFileStorage/common/exceptions/ErrorResponse.java
@@ -0,0 +1,24 @@
+package com.product.CloudFileStorage.common.exceptions;
+
+import java.time.Instant;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ErrorResponse {
+    private Instant timestamp;
+    private int status;
+    private String error;
+    private String message;
+    private String path;
+
+    public ErrorResponse(Instant timestamp, int status, String error, String message, String path) {
+        this.timestamp = timestamp;
+        this.status = status;
+        this.error = error;
+        this.message = message;
+        this.path = path;
+    }
+}
---
+++ src/main/java/com/product/CloudFileStorage/common/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,120 @@
+package com.product.CloudFileStorage.common.exceptions;
+
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.product.CloudFileStorage.common.exceptions.custom.AccessDeniedException;
+import com.product.CloudFileStorage.common.exceptions.custom.UserNotFoundException;
+import com.product.CloudFileStorage.user.exception.InvalidCredentialsException;
+import com.product.CloudFileStorage.user.exception.UserAlreadyExistsException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+/**
+ * Global exception handler for the application.
+ * Intercepts exceptions thrown across all controllers
+ * and returns a structured ErrorResponse with appropriate HTTP status codes.
+ */
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    /**
+     * Handles cases where the requested user is not found
+     * Returns 404 NOT_FOUND
+     */
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleUserNotFoundException(
+            UserNotFoundException ex,
+            HttpServletRequest request) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        ErrorResponse errorResponse = new ErrorResponse(
+                Instant.now(),
+                status.value(),
+                status.getReasonPhrase(),
+                ex.getMessage(),
+                request.getRequestURI());
+        return new ResponseEntity<>(errorResponse, status);
+    }
+
+    /**
+     * Handles cases where the user already exists
+     * Returns 409 CONFLICT
+     */
+
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public ResponseEntity<ErrorResponse> handleUserAlreadyExistsException(
+            UserAlreadyExistsException ex,
+            HttpServletRequest request) {
+        HttpStatus status = HttpStatus.CONFLICT;
+        ErrorResponse errorResponse = new ErrorResponse(
+                Instant.now(),
+                status.value(),
+                status.getReasonPhrase(),
+                ex.getMessage(),
+                request.getRequestURI());
+        return new ResponseEntity<>(errorResponse, status);
+    }
+
+    /**
+     * Handles when the user enters invalid login credentials
+     * Returns 401 UNAUTHORIZED
+     */
+
+    @ExceptionHandler(InvalidCredentialsException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidCredentialsException(
+            InvalidCredentialsException ex,
+            HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNAUTHORIZED;
+        ErrorResponse errorResponse = new ErrorResponse(
+                Instant.now(),
+                status.value(),
+                status.getReasonPhrase(),
+                ex.getMessage(),
+                request.getRequestURI());
+        return new ResponseEntity<>(errorResponse, status);
+    }
+
+    /**
+     * Handles when the user does not have access to the requested resource
+     * Returns 403 FORBIDDEN
+     */
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ErrorResponse> handleAccessDeniedException(
+            AccessDeniedException ex,
+            HttpServletRequest request) {
+        HttpStatus status = HttpStatus.FORBIDDEN;
+        ErrorResponse errorResponse = new ErrorResponse(
+                Instant.now(),
+                status.value(),
+                status.getReasonPhrase(),
+                ex.getMessage(),
+                request.getRequestURI());
+        return new ResponseEntity<>(errorResponse, status);
+    }
+
+    /**
+     * Handles all unexpected exceptions not caught by other handlers.
+     * Returns 500 Internal Server Error.
+     */
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(
+            Exception ex,
+            HttpServletRequest request) {
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+        ErrorResponse errorResponse = new ErrorResponse(
+                Instant.now(),
+                status.value(),
+                status.getReasonPhrase(),
+                ex.getMessage(),
+                request.getRequestURI());
+        return new ResponseEntity<>(errorResponse, status);
+    }
+}
