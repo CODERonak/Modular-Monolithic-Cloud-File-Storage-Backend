4e5efcb66cf159f5f70e4ce375dabd14ff22525d
r9988.2588 <r9988.2588@gmail.com>
2026-05-11 06:19:03 +0000

    feat(user): configure Spring Security with JWT util, filter and entry point

--- src/main/java/com/product/CloudFileStorage/user/security/config/SecurityConfig.java
+++ src/main/java/com/product/CloudFileStorage/user/security/config/SecurityConfig.java
@@ -2,6 +2,9 @@ package com.product.CloudFileStorage.user.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,11 +12,40 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.product.CloudFileStorage.user.security.jwt.JWTEntryPoint;
+import com.product.CloudFileStorage.user.security.jwt.JWTFilter;
+import com.product.CloudFileStorage.user.security.jwt.JWTUtil;
+import com.product.CloudFileStorage.user.service.impl.UserDetailsServiceImpl;
+
+import lombok.AllArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
+@AllArgsConstructor
 public class SecurityConfig {
+
+    private final UserDetailsServiceImpl userDetailsService;
+    private final JWTEntryPoint jwtEntryPoint;
+    private final JWTUtil jwtUtil;
+
+    // creates a new instance for the JWT Filter and validating JWT tokens
+    @Bean
+    public JWTFilter authenticationJwtTokenFilter() {
+        return new JWTFilter(jwtUtil, userDetailsService);
+    }
+
+    // authenticates users
+    @Bean
+    public AuthenticationManager authenticationManager() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider(userDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+
+        return new ProviderManager(authenticationProvider);
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -32,7 +64,15 @@ public class SecurityConfig {
                 .httpBasic(Customizer.withDefaults())
 
                 // configures the session management to be stateless
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                // configures jwt filter
+                .addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class)
+
+                // configures exception handling for jwt
+                .exceptionHandling(exceptionHandling -> exceptionHandling.authenticationEntryPoint(jwtEntryPoint))
+
+                .authenticationManager(authenticationManager());
 
         return http.build();
     }
