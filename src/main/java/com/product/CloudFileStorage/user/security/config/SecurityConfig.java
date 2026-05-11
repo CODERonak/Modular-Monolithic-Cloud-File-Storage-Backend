745493208cc2f04ffe9ac53681bdb136b0c52e4b
r9988.2588 <r9988.2588@gmail.com>
2026-05-11 05:24:26 +0000

    feat(user): add Spring Security config

---
+++ src/main/java/com/product/CloudFileStorage/user/security/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.product.CloudFileStorage.user.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                // authorizes request
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers(
+                                "/auth/**",
+                                "/swagger-ui/**",
+                                "/v3/api-docs*/**",
+                                "/swagger-ui.html")
+                        .permitAll()
+                        .anyRequest().authenticated())
+
+                // enables basic auth
+                .httpBasic(Customizer.withDefaults())
+
+                // configures the session management to be stateless
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+}
