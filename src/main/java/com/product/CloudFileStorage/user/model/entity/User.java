903104d66bae03b4b089abda1b86af5f057776c6
r9988.2588 <r9988.2588@gmail.com>
2026-05-11 03:53:28 +0000

    feat(user): add User entity and Role enum

---
+++ src/main/java/com/product/CloudFileStorage/user/model/entity/User.java
@@ -0,0 +1,42 @@
+package com.product.CloudFileStorage.user.model.entity;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import com.product.CloudFileStorage.user.model.enums.Role;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, name = "full_name")
+    private String fullname;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt = Instant.now();
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = Instant.now();
+    }
+}
---
+++ src/main/java/com/product/CloudFileStorage/user/model/enums/Role.java
@@ -0,0 +1,6 @@
+package com.product.CloudFileStorage.user.model.enums;
+
+public enum Role {
+    ADMIN,
+    USER
+}
