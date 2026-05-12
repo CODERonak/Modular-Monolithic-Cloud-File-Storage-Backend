5a78171195a98b8fe9287c196fd7033a0f27ac84
r9988.2588 <r9988.2588@gmail.com>
2026-05-12 03:33:47 +0000

    feat(file): add File entity with owner relation and storage metadata

---
+++ src/main/java/com/product/CloudFileStorage/file/model/entity/File.java
@@ -0,0 +1,49 @@
+package com.product.CloudFileStorage.file.model.entity;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import com.product.CloudFileStorage.user.model.entity.User;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "files")
+public class File {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "owner_id", nullable = false)
+    private User owner;
+
+    private UUID folderId;
+
+    @Column(nullable = false)
+    private String originalFileName;
+
+    @Column(nullable = false)
+    private String storageFileName;
+
+    @Column(nullable = false)
+    private String contentType;
+
+    @Column(nullable = false)
+    private Long size;
+
+    @Column(nullable = false)
+    private String fileUrl;
+
+    private Instant uploadedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.uploadedAt = Instant.now();
+    }
+}
